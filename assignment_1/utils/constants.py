11db26affc2508cd29a1d905dcc27b775f65c766
dipanshuporwal <dipanshunitj@gmail.com>
2025-08-13 13:53:52 +0530

    Delete assignment_1 directory

--- assignment_1/utils/constants.py
+++
@@ -1,4 +0,0 @@
-import os
-
-OUTPUT_DIR = "output"
-RESUME_OUTPUT_DIR = os.path.join(OUTPUT_DIR, "resumes")
--- assignment_1/utils/name_sanitizer.py
+++
@@ -1,5 +0,0 @@
-import re
-
-
-def sanitize_filename(name: str) -> str:
-    return re.sub(r"[^\w\-_.]", "_", name)
